63c77bfccd8061fbb12049e15655e997974ad349
ishidasb <93340982+ishidasb@users.noreply.github.com>
2024-02-02 23:52:26 -0500

    push
    test

--- Bluetooth connect Findall/Bluetooth connect Findall/App.xaml.cs
+++ Bluetooth connect Findall/Bluetooth connect Findall/App.xaml.cs
@@ -22,18 +22,6 @@ namespace Bluetooth_connect_Findall
 { 
     sealed partial class App : Application
     {
-        private Lazy<ActivationService> _activationService;
-
-        private ActivationService ActivationService
-        {
-            get { return _activationService.Value; }
-        }
-        public App()
-        {
-            UnhandledException += OnAppUnhandledException;
-            _activationService = new Lazy<ActivationService>(CreateActivationService);
-        }
-
 
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
@@ -85,48 +73,10 @@ namespace Bluetooth_connect_Findall
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-            protected override async void OnLaunched(LaunchActivatedEventArgs args)
-        {
-            if (!args.PrelaunchActivated)
-            {
-                await ActivationService.ActivateAsync(args);
-            }
-        }
-
-        protected override async void OnActivated(IActivatedEventArgs args)
-        {
-            await ActivationService.ActivateAsync(args);
-        }
-
         private void OnAppUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
         {
             // TODO: Please log and handle the exception as appropriate to your scenario
             // For more info see https://docs.microsoft.com/uwp/api/windows.ui.xaml.application.unhandledexception
         }
-
-        private ActivationService CreateActivationService()
-        {
-            return new ActivationService(this, typeof(Views.MainPage), new Lazy<UIElement>(CreateShell));
-        }
-
-        private UIElement CreateShell()
-        {
-            return new Views.ShellPage();
-        }
-    }
-}
     }
 }
